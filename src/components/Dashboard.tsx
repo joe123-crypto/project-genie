d8b498c5ade041789664b9810596bec7e8b7006c
munemojoseph332 <munemojoseph332@gmail.com>
2025-10-22 13:00:25 +0000

    fixing issues and improving features

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -90,7 +90,7 @@ const Dashboard: React.FC<DashboardProps> = ({ user, setViewState, addFilter }) 
           onChange={(e) => setText(e.target.value)}
           rows={1}
           placeholder="eg Make the person in the image look old(be specific)"
-          className="w-full bg-transparent focus:outline-none text-content-100 dark:text-dark-content-100 px-4 placeholder-opacity-50 resize-none overflow-y-hidden"
+          className="w-full bg-transparent focus:outline-none text-content-100 dark:text-dark-content-100 px-4 resize-none overflow-y-hidden placeholder-gray-500 dark:placeholder-gray-400"
           disabled={isLoading}
         />
         <button className="p-2 text-content-200 hover:text-brand-primary dark:text-dark-content-200 dark:hover:text-dark-brand-primary rounded-full transition-colors" disabled={isLoading}>
