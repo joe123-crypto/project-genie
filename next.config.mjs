348847d13eedc0bb04381a2a0b4887ba75367d80
munemojoseph332 <munemojoseph332@gmail.com>
2025-11-04 01:38:25 +0000

    fixing image previews

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        loader: 'custom',
+        loaderFile: './loader.js',
         remotePatterns: [
             {
                 protocol: 'https',
